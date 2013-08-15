c4cfc22f6e67dd23c71b9e3c8e794f1ad77fbc40
Kirill Mukhov <dissdoc@gmail.com>
2013-08-15 18:48:00 +0400

    add click left-right buttons

--- js/initialize.js
+++ js/initialize.js
@@ -3,6 +3,41 @@ calendar.generate();
 calendar.initDaysOfWeek();
 
 var date = new Calendar.Dater.Datetime();
-document.getElementsByClassName('month-title')[0].innerText = date.getHeaderDate();
 
-calendar.fillDays(date.getDays(date.getYear(), date.getMonth(), date.getDay()), date.getDay());
+var day = date.getDay();
+var month = date.getMonth();
+var year = date.getYear();
+
+var headerMonth = document.getElementsByClassName('month-title')[0];
+headerMonth.innerText = date.getHeaderDate(month, year);
+
+calendar.fillDays(date.getDays(year, month, day), day);
+
+var rightBtn = document.getElementById('right-button');
+var leftBtn = document.getElementById('left-button');
+
+rightBtn.addEventListener('click', function(e) {
+	if (month == 11) {
+		month = 0;
+		year++;
+	} else {
+		month++;
+	}
+	calendar.clearDays();
+	calendar.initDaysOfWeek();
+	calendar.fillDays(date.getDays(year, month, day), 0);	
+	headerMonth.innerText = date.getHeaderDate(month, year);	
+});
+
+leftBtn.addEventListener('click', function(e) {
+	if (month == 0) {
+		month = 11;
+		--year;
+	} else {
+		month--;
+	}
+	calendar.clearDays();
+	calendar.initDaysOfWeek();
+	calendar.fillDays(date.getDays(year, month, day), 0);
+	headerMonth.innerText = date.getHeaderDate(month, year);
+});
